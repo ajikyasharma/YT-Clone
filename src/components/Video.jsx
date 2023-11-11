07ab01bb15e2d5964ad1e1f72349f64ca7fc5dea
ajikyasharma <ajikyasharma24@gamil.com>
2023-11-11 10:42:22 +0530

    Update video list UI

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -6,10 +6,10 @@ const Video =()=>{
 
     return <div className="mb-3 mt-3">
         <Row>
-            <Col xs="7">
+            <Col xs={7}>
                   <img src="https://images.unsplash.com/photo-1493193218435-eb21654c7df6?w=180&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE5fHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D"/>
             </Col>
-            <Col xs="5">
+            <Col xs={5}>
                <h4>Welcome to my  Youtube</h4>
             </Col>
         </Row>
