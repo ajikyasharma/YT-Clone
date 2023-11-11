b93c76bcd853b8266f6968f69088e6c476545f75
ajikyasharma <ajikyasharma24@gamil.com>
2023-11-11 11:04:59 +0530

    Update Video List

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,16 +1,17 @@
 
 import { Container, Row,Col } from "react-bootstrap"
+import Image from 'react-bootstrap/Image';
 
 
 const Video =()=>{
 
     return <div className="mb-3 mt-3">
         <Row>
-            <Col xs={7}>
-                  <img src="https://images.unsplash.com/photo-1493193218435-eb21654c7df6?w=180&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE5fHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D"/>
+            <Col xs={8} md={8} lg={8}>
+            <Image fluid src="https://images.unsplash.com/photo-1493193218435-eb21654c7df6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE5fHhqUFI0aGxrQkdBfHxlbnwwfHx8fHw%3D" />
             </Col>
-            <Col xs={5}>
-               <h4>Welcome to my  Youtube</h4>
+            <Col xs={4} md={4} lg={4}>
+               <p>Welcome Youtube</p>
             </Col>
         </Row>
         
